80b0f7335783780dc10eeba31bf2c2f59b0fb6e2
nitgeek <nitgeek@outlook.com>
2021-11-23 07:57:24 +0530

    egress-e2e tls

---
+++ 12-egress-communication-e2e-tls/7-cleanup.sh
@@ -0,0 +1,8 @@
+kubectl delete secret egress-mutual-credential -n istio-system;
+kubectl delete secret mutual-credential -n istio-system;
+kubectl delete -f istio-setup/ingress-gateway-tls.yaml;
+kubectl delete -f istio-setup/service-entry.yaml;
+kubectl delete -f istio-setup/egress-gateway.yaml;
+kubectl delete -f istio-setup/egress-dr.yaml;
+kubectl delete -f istio-setup/vs-egress.yaml;
+kubectl delete -f istio-setup/egress-dr-egress-svc.yaml;
